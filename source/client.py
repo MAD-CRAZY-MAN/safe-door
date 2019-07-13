0312d7d51e6319f1a4c9ab228dcf07392b846714
MAD-CRAZY-MAN <tlgustlgus0425@naver.com>
2019-07-14 02:08:16 +0900

    feat: save image

--- source/client.py
+++ source/client.py
@@ -4,27 +4,37 @@ import cv2
 import numpy
 import serial
 
-ser = serial.Serial('/dev/ttyACM0', 9600) # serial connect
+def init():
+    global TCP_IP, TCP_PORT
+    global sock, ser
+    global encode_param, capture
+    
+    TCP_IP = '172.30.1.31' # server socket`s IP
+    TCP_PORT = 5001        # server socket's PORT
 
-TCP_IP = '172.30.1.31' # server socket`s IP
-TCP_PORT = 5001        # server socket's PORT
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Create socket, IP4v
+    sock.connect((TCP_IP, TCP_PORT)) # connect socket
 
+    ser = serial.Serial('/dev/ttyACM0', 9600) # serial connect
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Create socket, IP4v
-sock.connect((TCP_IP, TCP_PORT)) # connect socket
+    encode_param=[int(cv2.IMWRITE_JPEG_QUALITY),90] # encode jpg, img quality value
+    capture = cv2.VideoCapture(0) # Create Video object No. 0 index
 
-encode_param=[int(cv2.IMWRITE_JPEG_QUALITY),90] # encode jpg, img quality value
-capture = cv2.VideoCapture(0) # Create Video object No. 0 index
+    return None
+
+init()
 
 while True:
-    response = ser.read # read data from arduino
+    response = ser.read(1) # read data from arduino
     if response = '1':
         ret, frame = capture.read() # read 1 frame, return : True or Faluse, read frame
         result, imgencode = cv2.imencode('.jpg', frame, encode_param) # incode to jpg, at frame, above special type
-        data = numpy.array(imgencode)
-        stringData = data.tostring()
+
+        data = numpy.array(imgencode) # 배열을 만듬
+        stringData = data.tostring() # 자료형을 문자열로 변환
+
         sock.send( str(len(stringData)).ljust(16)) # 읽은 데이터의 길이를 스트링으로 변환하고 16바이트에서 왼쪽 정렬
-        sock.send( stringData ) # send to socket
+        sock.send( stringData ) # send an encodeing image to socket
 
 
 sock.close() # close socket
--- source/server.py
+++ source/server.py
@@ -1,34 +1,49 @@
 import socket
 import cv2
 import numpy
+from datetime import datetime
 
-def recvall(sock, count):
+def recvall(sock, count): # count만큼 데이터를 읽어서 buf에 저장하고 반환하는 함수
     buf = b''
     while count:
-        newbuf = sock.recv(count)
-        if not newbuf: return None
+        newbuf = sock.recv(count) # count byte만큼 데이터를 읽어서 저장한다
+        if not newbuf: return None # newbuf에 저장된 값이 없으면 결과 값없음을 반환
         buf += newbuf
-        count -= len(newbuf)
+        count -= len(newbuf) # 함수를 빠져나감 
     return buf
 
-TCP_IP = '172.30.1.31' # server socket`s IP
-TCP_PORT = 20000       # server socket`s PORT
-
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Create socket 
-sock.bind((TCP_IP, TCP_PORT)) # bind socket to IP, port
-sock.listen(True) # wait client
-client, addr = s.accept() # accept client, client = clilent`s socket, addr = client`s address 
-
-length = recvall(client,16) # read
-stringData = recvall(client, int(length))
-data = numpy.fromstring(stringData, dtype='uint8')
-sock.close()
-
-decimg=cv2.imdecode(data,1) # decoding
-cv2.imshow('SERVER',decimg) # show image
-
-k = cv2.waitKey(0)
-if k == 27:
-    cv2.destroyAllWindows()
+def init():
+    global TCP_IP, TCP_PORT
+    global sock, client
     
-cv2.destroyAllWindows() 
+    TCP_IP = '172.30.1.31' # server socket`s IP
+    TCP_PORT = 20000       # server socket`s PORT
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Create socket 
+    sock.bind((TCP_IP, TCP_PORT)) # bind socket to IP, port
+    sock.listen(1) # wait client
+    client, addr = sock.accept() # accept client, client = clilent`s socket, addr = client`s address 
+
+    return None
+
+init()
+
+while True:
+    length = recvall(client,16) # read (socket, bytes), length = data`s size
+    stringData = recvall(client, int(length)) # 이미지의 길이만큼 데이터를 읽음
+    data = numpy.fromstring(stringData, dtype='uint8') # 문자열 자료형으로부터 unsinged int형으로 변환
+
+    decimg=cv2.imdecode(data, cv2.CV_LOAD_IMAGE_COLOR) # decoding
+    cv2.imshow('CCTV', decimg) # show image
+    now = datetime.now()
+    cv2.imwrite('C:/list/' + now + '.jpg', decimg) # 이미지 저장 
+
+sock.close()
+# 사용자가 클릭해서 닫을 수 있도록 하자
+'''
+while True: # esc를 누를 때 까지 대기
+    k = cv2.waitKey(0) 
+    if k == 27:
+        cv2.destroyAllWindows()
+        break;
+'''
