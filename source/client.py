206069ce9bb3bce15f5fdf539a76a56a3ff6e2e8
MAD-CRAZY-MAN <tlgustlgus0425@naver.com>
2019-07-13 23:43:10 +0900

    Add: client.py

---
+++ source/client.py
@@ -0,0 +1,37 @@
+# 함수 화 하기, 이미지 저장(시간)
+import socket
+import cv2
+import numpy
+import serial
+
+ser = serial.Serial('/dev/ttyACM0', 9600) # serial connect
+
+TCP_IP = '172.30.1.31' # server socket`s IP
+TCP_PORT = 5001        # server socket's PORT
+
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Create socket
+sock.connect((TCP_IP, TCP_PORT)) # connect socket
+
+encode_param=[int(cv2.IMWRITE_JPEG_QUALITY),90] # encode jpg, img quality value
+capture = cv2.VideoCapture(0) # Create Video object No. 0 index
+
+while True:
+    response = ser.read # read data from arduino
+    if response = '1':
+        ret, frame = capture.read() # read 1 frame, return : True or Faluse, read frame
+        result, imgencode = cv2.imencode('.jpg', frame, encode_param) # incode to jpg, at frame, above special type
+        data = numpy.array(imgencode)
+        stringData = data.tostring()
+        sock.send( str(len(stringData)).ljust(16)) # 읽은 데이터의 길이를 스트링으로 변환하고 16바이트에서 왼쪽 정렬
+        sock.send( stringData ) # send to socket
+
+
+sock.close() # close socket
+
+'''
+decimg=cv2.imdecode(data,1)
+cv2.imshow('CLIENT',decimg)
+cv2.waitKey(0)
+cv2.destroyAllWindows() 
+'''
